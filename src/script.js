339f4d0652b2614f0fa32416b42bd56058501dbf
Juan Montero <juanca.montero76@gmail.com>
2024-07-24 10:08:57 -0500

    to test

--- src/script.js
+++ src/script.js
@@ -24,32 +24,39 @@ const gltfLoader = new GLTFLoader()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    pixelRatio: Math.min(window.devicePixelRatio, 2)
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(window.devicePixelRatio, 2),
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // *Update materials
+  material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(sizes.pixelRatio)
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
 camera.position.x = 7
 camera.position.y = 7
 camera.position.z = 7
@@ -66,19 +73,16 @@ const rendererParameters = {}
 rendererParameters.clearColor = '#26132f'
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true,
 })
 renderer.setClearColor(rendererParameters.clearColor)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
-gui
-    .addColor(rendererParameters, 'clearColor')
-    .onChange(() =>
-    {
-        renderer.setClearColor(rendererParameters.clearColor)
-    })
+gui.addColor(rendererParameters, 'clearColor').onChange(() => {
+  renderer.setClearColor(rendererParameters.clearColor)
+})
 
 /**
  * Material
@@ -87,87 +91,81 @@ const materialParameters = {}
 materialParameters.color = '#ff794d'
 
 const material = new THREE.ShaderMaterial({
-    vertexShader: halftoneVertexShader,
-    fragmentShader: halftoneFragmentShader,
-    uniforms:
-    {
-        uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
-        uShadeColor: new THREE.Uniform(new THREE.Color(materialParameters.shadeColor)),
-    }
+  vertexShader: halftoneVertexShader,
+  fragmentShader: halftoneFragmentShader,
+  uniforms: {
+    uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
+    uShadeColor: new THREE.Uniform(
+      new THREE.Color(materialParameters.shadeColor)
+    ),
+    //todo: update en window resize
+    uResolution: new THREE.Uniform(
+      new THREE.Vector2(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio
+      )
+    ), // se necesita para normalizar gl_FragCoord.xy
+  },
 })
 
-gui
-    .addColor(materialParameters, 'color')
-    .onChange(() =>
-    {
-        material.uniforms.uColor.value.set(materialParameters.color)
-    })
+gui.addColor(materialParameters, 'color').onChange(() => {
+  material.uniforms.uColor.value.set(materialParameters.color)
+})
 
 /**
  * Objects
  */
 // Torus knot
 const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
-    material
+  new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+  material
 )
 torusKnot.position.x = 3
 scene.add(torusKnot)
 
 // Sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(),
-    material
-)
-sphere.position.x = - 3
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(), material)
+sphere.position.x = -3
 scene.add(sphere)
 
 // Suzanne
 let suzanne = null
-gltfLoader.load(
-    './suzanne.glb',
-    (gltf) =>
-    {
-        suzanne = gltf.scene
-        suzanne.traverse((child) =>
-        {
-            if(child.isMesh)
-                child.material = material
-        })
-        scene.add(suzanne)
-    }
-)
+gltfLoader.load('./suzanne.glb', (gltf) => {
+  suzanne = gltf.scene
+  suzanne.traverse((child) => {
+    if (child.isMesh) child.material = material
+  })
+  scene.add(suzanne)
+})
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Rotate objects
-    if(suzanne)
-    {
-        suzanne.rotation.x = - elapsedTime * 0.1
-        suzanne.rotation.y = elapsedTime * 0.2
-    }
+  // Rotate objects
+  if (suzanne) {
+    suzanne.rotation.x = -elapsedTime * 0.1
+    suzanne.rotation.y = elapsedTime * 0.2
+  }
 
-    sphere.rotation.x = - elapsedTime * 0.1
-    sphere.rotation.y = elapsedTime * 0.2
+  sphere.rotation.x = -elapsedTime * 0.1
+  sphere.rotation.y = elapsedTime * 0.2
 
-    torusKnot.rotation.x = - elapsedTime * 0.1
-    torusKnot.rotation.y = elapsedTime * 0.2
+  torusKnot.rotation.x = -elapsedTime * 0.1
+  torusKnot.rotation.y = elapsedTime * 0.2
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -1,16 +1,68 @@
 uniform vec3 uColor;
+uniform vec2 uResolution;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
 
-void main()
-{
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
+
+void main() {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
 
+    // * Lights
+    vec3 light = vec3(0.0);
+
+    light += ambientLight(vec3(1.0), // Light color
+    1.0        // Light intensity,
+    );
+
+    light += directionalLight(vec3(1.0, 1.0, 1.0), // Light color
+    1.0,                 // Light intensity
+    normal,              // Normal
+    vec3(1.0, 1.0, 0.0), // Light position
+    viewDirection,       // View direction
+    1.0                  // Specular power
+    );
+
+    color *= light; //must be black, lights are off
+
+    // * HALF TONE
+
+    // * Grid
+    //? la idea es tener un GRID de puntos que se quede estático 
+    //? y que NO siga a los objetos o a las transformaciones de la cámara
+    //? similar a "display:fixed;" en ccs
+    //? se necesitan entonces las coordenadas  UV de todo el RENDER
+    //? Existe gl_FragCoord que justo entrega: X y Y para 2D y  Z y W adicionales para 3D
+    //? solo vamos a usar 2D
+    //? las ccordenadas xy van como en un plano cartesiano siendo cero la esquina izq inferior
+
+    //! los valores de gl_FragCoord varían hasta los valores del "número de pixeles", por eso si divide por 1200.0
+    //  vec2 uv = gl_FragCoord.xy / 1200.0; 
+
+    // //? para normalizar se trae la resolución como una uniforme dentro de un vec2
+    //  vec2 uv = gl_FragCoord.xy / uResolution.y;  //? solo utilizar Y para escalar igual todos los vectores
+    //  //? para crear la grilla se precisa que el UV se vuelva repetitivo 0 a 1, 0 a 1, segun la posición
+    //  uv *= 50.0; //? al multiplicar por este valor incrmento la frecuencia de las coordenadas, creo más divisiones
+    //  uv = mod(uv, 1.0); //? con el módulo lo hago repetitivo cada 1.0
+
+    float repetitions = 50.0;
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv *= repetitions;
+    uv = mod(uv, 1.0);
+
+    // * Points
+    //? the uv coord is the left bottom of the cell , so a Distance to a vec2(0.5) returns a circle
+    float point = distance(uv, vec2(0.5));
+
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(uv, 1.0, 1.0);
+    gl_FragColor = vec4(point, point, point, 1.0);
+    // gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
